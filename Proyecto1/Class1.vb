2183fca62d0ab695b36a16c3cdb9a35a65959e0f
JosephJCardenas <joseph272011@hotmail.com>
2021-10-01 22:21:49 -0500

    Creada clase 1

---
+++ Proyecto1/Class1.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+
+Public Class MyVerticalProgessBar
+    Inherits ProgressBar
+    Protected Overloads Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            Dim pb As CreateParams = MyBase.CreateParams
+            pb.Style = pb.Style Or &H6
+            Return pb
+        End Get
+    End Property
+End Class
