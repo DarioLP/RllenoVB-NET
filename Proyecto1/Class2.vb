2df1009f687d45c9c380e89eb163c915b8cca5ed
DarioLP <lopezdario43@gmail.com>
2021-10-02 12:38:37 -0500

    joseph version

---
+++ Proyecto1/Class2.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Imports System.Windows.Forms
+
+Public Class MyVerticalProgessBar
+    Inherits ProgressBar
+    Protected Overloads Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            Dim bp As CreateParams = MyBase.CreateParams
+            bp.Style = bp.Style Or &H4
+            Return bp
+        End Get
+    End Property
+End Class
--- Proyecto1/Form1.vb
+++ Proyecto1/Form1.vb
@@ -1,3 +1,43 @@
 ﻿Public Class Form1
-    'hola mundo
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+        TextBox1.Text = MyVerticalProgessBar1.Value
+        TextBox2.Text = MyVerticalProgessBar2.Value
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim F As Integer
+        Dim guardar As Integer
+        Timer1.Start()
+        MyVerticalProgessBar1.Value = Timer1.Interval
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim F As Integer
+        F = 0
+        If F = 0 Then
+            For i As Integer = 0 To 5000
+                MyVerticalProgessBar1.Value = 5000 - 1
+            Next
+            For i As Integer = 0 To 5000
+                MyVerticalProgessBar2.Value += 1
+            Next
+        End If
+    End Sub
+
+    Private Sub HScrollBar1_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar1.Scroll
+        If HScrollBar1.Left Then
+            Timer1.Start()
+        Else
+            If HScrollBar1.RightToLeft Then
+                Timer1.Interval = 1 + 5000
+            End If
+        End If
+
+    End Sub
 End Class
